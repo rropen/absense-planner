f83dd2c98db57cb51d9e6cd43ced5c04ed5b75af
Basel Akasha <baselakasha@gmail.com>
2022-02-10 18:53:12 +0000

    Add a Bat script that can setup dev enviroment

--- absence_planner/settings.py
+++ absence_planner/settings.py
@@ -124,13 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = str(BASE_DIR) + "/static_root"
 
-STATICFILES_DIRS = [ 
-    str(BASE_DIR) + "/plannerapp/static" 
+STATICFILES_DIRS = [
+    str(BASE_DIR) + "/plannerapp/static"
 ]
-
-print(str(BASE_DIR) + "/plannerapp/static" )
-
 
 
 # Default primary key field type
