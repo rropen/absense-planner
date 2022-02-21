be68d3f7195f9fe4b708fc088df9d48602927055
Jai <829138@student.derby-college.ac.uk>
2022-02-21 12:14:43 +0000

    adding models

--- plannerapp/models.py
+++ plannerapp/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class employees(models.Model):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.firstName
+
+
+class absences(models.Model):
+    absenceID = models.AutoField(primary_key=True)
+    date = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.date
