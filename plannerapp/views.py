35faec954d069d35fcde80eb62736d3844a96a83
James Wheeler <93123413+J-Wheeler849277@users.noreply.github.com>
2022-02-21 15:42:05 +0000

    Updated the calendar and navbar

--- plannerapp/views.py
+++ plannerapp/views.py
@@ -94,7 +94,7 @@ def sign_up_page(response):
 
 
 def calendar(response):
-    number = [1,2,3,4,5,6,7,8,9]
+    number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
     return render(response, "plannerapp/Calendar.html", {"num": number})
 
 
