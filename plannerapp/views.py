4ba9de8f50e4ab92658256ac7a2396bfdb05b2c6
Jai <829138@student.derby-college.ac.uk>
2022-02-21 11:49:29 +0000

    added superuser

--- plannerapp/views.py
+++ plannerapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import names, login, sign_up
 
 # Temp
 
@@ -19,46 +19,10 @@ profiles_info = []  # Not too sure how these details are going to be laid out
 
 selected_date = "29/10/21"
 
-
-def details_page(request):
-    context = {
-        "employee_dicts": details_info,
-    }
-    return render(request, "plannerapp/Details.html", context)
-
-
-def calendar_page(request):
-    context = {
-        "dates": calendar_info
-    }  # Needs to take the list which contains that dates information
-
-    print("test")
-
-    return render(request, "plannerapp/Calendar.html", context)
-
-
-# Profile page
-
 selected_name = "bob"
 job_role = "Software Developer"  # Not sure if this is neccessary
 project_id = "AB12341"
 todays_attendance = False
-
-
-def profile_page(request):
-    selected_profile_context = {
-        "name": selected_name,
-        "job_role": job_role,
-        "project_id": project_id,
-        "attending": todays_attendance,
-    }
-
-    return render(request, "plannerapp/Profile.html", selected_profile_context)
-
-
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .forms import names, login, sign_up
 
 
 def index(response):
@@ -91,3 +55,32 @@ def nameForm(request):
     context = {"form": form}
 
     return render(request, "plannerapp/form.html", context)
+
+
+def details_page(request):
+    context = {
+        "employee_dicts": details_info,
+    }
+    return render(request, "plannerapp/Details.html", context)
+
+
+def calendar_page(request):
+    context = {
+        "dates": calendar_info
+    }  # Needs to take the list which contains that dates information
+
+    print("test")
+
+    return render(request, "plannerapp/Calendar.html", context)
+
+
+# Profile page
+def profile_page(request):
+    selected_profile_context = {
+        "name": selected_name,
+        "job_role": job_role,
+        "project_id": project_id,
+        "attending": todays_attendance,
+    }
+
+    return render(request, "plannerapp/Profile.html", selected_profile_context)
