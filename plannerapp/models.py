63f56a38520ecb7a3e5d06907a1f04b7f95ca613
Matthew Palmer <93127727+student833497@users.noreply.github.com>
2022-05-20 12:10:20 +0100

    Calendar Integration with Teams
    No Overall Calendar atm but will be implemented within the future. Also removed fields from the Absence model

--- plannerapp/models.py
+++ plannerapp/models.py
@@ -8,12 +8,9 @@ class Absence(models.Model):
     User_ID = models.ForeignKey(User, on_delete=models.CASCADE, related_name="absences")
     absence_date_start = models.DateField(max_length=200)
     absence_date_end = models.DateField(max_length=200)
-    request_date = models.DateField(default=timezone.now)
-    manager_ID = models.CharField(max_length=200)
-    request_accepted = models.BooleanField()
 
     def __str__(self):
-        return f"{self.User_ID}, {self.absence_date_start}"
+        return f"{self.User_ID}, {self.absence_date_start} - {self.absence_date_end}"
 
 
 class Team(models.Model):
