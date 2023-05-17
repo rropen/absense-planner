3b856b971db714edfb099e5327c15daa44d3a235
Patrick <patrickwigley2@gmail.com>
2023-05-17 21:26:59 +0100

    Example - Pytest with selenium

--- ap_src/ap_app/tests.py
+++ ap_src/ap_app/tests.py
@@ -2,7 +2,6 @@ import pytest
 from django.test import LiveServerTestCase
 from seleniumbase import SB
 
-
 #TODO: Add id's to web-page elements being used during testing, instead of using xPaths 
 #NOTE: 
 # - If executing in terminal with pytest, must run command inside "ap_src" directory - (where "pytest.ini" is located)
@@ -22,7 +21,7 @@ class TestBasicTemplate(LiveServerTestCase):
             "[Application]: Running SeleniumBase Commands Now" \
             f"LiveServer running on: {self.live_server_url}"
             )
-        
+
         with SB(demo=DEMO, headless=(DEMO is False)) as sb:
             sb.open(self.live_server_url)
             
@@ -31,4 +30,3 @@ class TestBasicTemplate(LiveServerTestCase):
             sb.assert_true("Home" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Home-Page - Title does not match")   
             # FAIL expected
             sb.assert_true("This is not in title" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Home-Page - Title does not match") 
-            
