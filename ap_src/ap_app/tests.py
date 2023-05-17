46ca504c31dbcabd00642ed715dd332e38352b01
Patrick <patrickwigley2@gmail.com>
2023-05-17 21:10:57 +0100

    Example - Pytest with selenium

--- ap_src/ap_app/tests.py
+++ ap_src/ap_app/tests.py
@@ -1,92 +1,34 @@
-from django.test import LiveServerTestCase
-# Pip install seleniumbase - "python -m pip install selenium-base"
-#from seleniumbase import Driver
-from seleniumbase import SB
 import pytest
+from django.test import LiveServerTestCase
+from seleniumbase import SB
+
+
+#TODO: Add id's to web-page elements being used during testing, instead of using xPaths 
+#NOTE: 
+# - If executing in terminal with pytest, must run command inside "ap_src" directory - (where "pytest.ini" is located)
+# - Example in terminal: C:\[BASE_DIR]\ap_src> pytest
+# main.yml does this automatically in CI pipeline. 
+
+# LiveServerTestCase is used to run the django application on an alternative thread, while tests are being executed
+# SB - Driver
 
 # Consts 
-URL = "http://LocalHost:8000"
-
-# Hard-Coded temp values
-USERNAME = "patrick"
-PASSWORD = "password"
-
-#NOTE: add id's instead of using xPaths
-
-
 DEMO = False
 
 
-class HostTest(LiveServerTestCase):
-    def test_basic(self):
-        print("[Application]: Running SeleniumBase Commands Now")
-        print(self.live_server_url)
-        with SB(demo=DEMO, headless=(DEMO is False)) as sb:
-            
-            sb.open(self.live_server_url)
-            print("Home-page title is: " + sb.get_page_title())
-            sb.assert_true("Home" in sb.get_page_title())   # PASS expected
-            #sb.assert_true("Home - RR Absence #" in sb.get_page_title()) # FAIL expected
-            
-
-            # -=-= Sign Up =-=- 
-
-            # sb.click_xpath("/html/body/container/div/container/div/a")
-
-            # # -=-= Login page =-=- 
-            # sb.assert_true("Login" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Login-Page")
-
-            # sb.send_keys("/html/body/container/div/div/form/div[1]/div/input", text=USERNAME)
-            # sb.send_keys("/html/body/container/div/div/form/div[2]/div/input", text=PASSWORD)
-            # # Click "Login"
-            # sb.click_xpath("/html/body/container/div/div/form/button")
-
-
-            # if "Policy" in sb.get_page_title():
-            #     # -=-= Policy Page =-=-
-            #     sb.click_xpath("/html/body/container/div/div/form/input[2]")
-            #     sb.click_xpath("/html/body/container/div/div/form/button")
-
-            # # -=-= Main Calendar Page =-=-
-            # sb.click_xpath("/html/body/nav/div[2]/div[1]/a[3]")
-
-            # -=-= Teams Page =-=-
-            sb.wait(8)
-
-    
-
-#with SB() as sb:
-#    print("[Application]: Running SeleniumBase Commands Now")
-#    sb.open(URL)
-#    sb.assert_title("Home - RR Absence")
-#    sb.click_xpath("/html/body/container/div/container/div/a")
-#
-#    # -=-= Login page =-=- 
-#    sb.assert_true("Login" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Login-Page")
-#    
-#    sb.send_keys("/html/body/container/div/div/form/div[1]/div/input", text=USERNAME)
-#    sb.send_keys("/html/body/container/div/div/form/div[2]/div/input", text=PASSWORD)
-#    # Click "Login"
-#    sb.click_xpath("/html/body/container/div/div/form/button")
-#
-#
-#    if "Policy" in sb.get_page_title():
-#        # -=-= Policy Page =-=-
-#        sb.click_xpath("/html/body/container/div/div/form/input[2]")
-#        sb.click_xpath("/html/body/container/div/div/form/button")
-#    
-#    # -=-= Main Calendar Page =-=-
-#    sb.click_xpath("/html/body/nav/div[2]/div[1]/a[3]")
-#
-#    # -=-= Teams Page =-=-
-    
-#change
-
-    #sb.wait(8)
-
-
-#class BasicTest(BaseCase):
-#    def test_basic(sb):
-#        sb.open(URL)
-#        sb.assert_title("Home - RR Absence")
+class TestBasicTemplate(LiveServerTestCase):
+    def test_example(self):
+        print(
+            "[Application]: Running SeleniumBase Commands Now" \
+            f"LiveServer running on: {self.live_server_url}"
+            )
         
+        with SB(demo=DEMO, headless=(DEMO is False)) as sb:
+            sb.open(self.live_server_url)
+            
+            # Basic test examples
+            # PASS expected
+            sb.assert_true("Home" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Home-Page - Title does not match")   
+            # FAIL expected
+            sb.assert_true("This is not in title" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Home-Page - Title does not match") 
+            
