85f4d77418ac13332bb8a54c984b42d8f76d9e5a
Patrick <patrickwigley2@gmail.com>
2023-05-17 19:52:08 +0100

    Test Commit - Expected pass on selenium test

--- ap_src/ap_app/tests.py
+++ ap_src/ap_app/tests.py
@@ -1,11 +1,11 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
 # Pip install seleniumbase - "python -m pip install selenium-base"
-#from seleniumbase import BaseCase
+#from seleniumbase import Driver
 from seleniumbase import SB
 import pytest
 
 # Consts 
-URL = "http://LocalHost:8000/"
+URL = "http://LocalHost:8000"
 
 # Hard-Coded temp values
 USERNAME = "patrick"
@@ -14,34 +14,74 @@ PASSWORD = "password"
 #NOTE: add id's instead of using xPaths
 
 
-with SB() as sb:
-    print("[Application]: Running SeleniumBase Commands Now")
-    sb.open(URL)
-    sb.assert_title("Home - RR Absence")
-    sb.click_xpath("/html/body/container/div/container/div/a")
+DEMO = False
 
-    # -=-= Login page =-=- 
-    sb.assert_true("Login" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Login-Page")
+
+class HostTest(LiveServerTestCase):
+    def test_basic(self):
+        print("[Application]: Running SeleniumBase Commands Now")
+        print(self.live_server_url)
+        with SB(demo=DEMO, headless=(DEMO is False)) as sb:
+            
+            sb.open(self.live_server_url)
+            sb.assert_true("Home - RR Absence" in sb.get_page_title())   # PASS expected
+            #sb.assert_true("Home - RR Absence #" in sb.get_page_title()) # FAIL expected
+            
+
+            # -=-= Sign Up =-=- 
+
+            # sb.click_xpath("/html/body/container/div/container/div/a")
+
+            # # -=-= Login page =-=- 
+            # sb.assert_true("Login" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Login-Page")
+
+            # sb.send_keys("/html/body/container/div/div/form/div[1]/div/input", text=USERNAME)
+            # sb.send_keys("/html/body/container/div/div/form/div[2]/div/input", text=PASSWORD)
+            # # Click "Login"
+            # sb.click_xpath("/html/body/container/div/div/form/button")
+
+
+            # if "Policy" in sb.get_page_title():
+            #     # -=-= Policy Page =-=-
+            #     sb.click_xpath("/html/body/container/div/div/form/input[2]")
+            #     sb.click_xpath("/html/body/container/div/div/form/button")
+
+            # # -=-= Main Calendar Page =-=-
+            # sb.click_xpath("/html/body/nav/div[2]/div[1]/a[3]")
+
+            # -=-= Teams Page =-=-
+            sb.wait(8)
+
     
-    sb.send_keys("/html/body/container/div/div/form/div[1]/div/input", text=USERNAME)
-    sb.send_keys("/html/body/container/div/div/form/div[2]/div/input", text=PASSWORD)
-    # Click "Login"
-    sb.click_xpath("/html/body/container/div/div/form/button")
 
-
-    if "Policy" in sb.get_page_title():
-        # -=-= Policy Page =-=-
-        sb.click_xpath("/html/body/container/div/div/form/input[2]")
-        sb.click_xpath("/html/body/container/div/div/form/button")
-    
-    # -=-= Main Calendar Page =-=-
-    sb.click_xpath("/html/body/nav/div[2]/div[1]/a[3]")
-
-    # -=-= Teams Page =-=-
+#with SB() as sb:
+#    print("[Application]: Running SeleniumBase Commands Now")
+#    sb.open(URL)
+#    sb.assert_title("Home - RR Absence")
+#    sb.click_xpath("/html/body/container/div/container/div/a")
+#
+#    # -=-= Login page =-=- 
+#    sb.assert_true("Login" in sb.get_page_title(), msg="[TESTING CODE ERROR]: Not on Login-Page")
+#    
+#    sb.send_keys("/html/body/container/div/div/form/div[1]/div/input", text=USERNAME)
+#    sb.send_keys("/html/body/container/div/div/form/div[2]/div/input", text=PASSWORD)
+#    # Click "Login"
+#    sb.click_xpath("/html/body/container/div/div/form/button")
+#
+#
+#    if "Policy" in sb.get_page_title():
+#        # -=-= Policy Page =-=-
+#        sb.click_xpath("/html/body/container/div/div/form/input[2]")
+#        sb.click_xpath("/html/body/container/div/div/form/button")
+#    
+#    # -=-= Main Calendar Page =-=-
+#    sb.click_xpath("/html/body/nav/div[2]/div[1]/a[3]")
+#
+#    # -=-= Teams Page =-=-
     
 #change
 
-    sb.wait(8)
+    #sb.wait(8)
 
 
 #class BasicTest(BaseCase):
