816caf90b71ada349dc0a0f4422a089a5d60737c
Patrick-Wigley <patrickwigley2@gmail.com>
2023-05-18 14:57:08 +0100

    nc

--- ap_src/ap_app/tests.py
+++ ap_src/ap_app/tests.py
@@ -6,6 +6,8 @@ from seleniumbase import BaseCase
 #NOTE: 
 # - If executing in terminal with pytest, must run command inside "ap_src" directory - (where "pytest.ini" is located)
 # - Example in terminal: C:\[BASE_DIR]\ap_src> pytest
+# - Although using Coverage is recommended as will produces a report of lines covered in testing suite
+# - Example in terminal using coverage: C:\[BASE_DIR]> pytest --cov=ap_app ap_src/ap_app
 # main.yml does this automatically in CI pipeline. 
 
 # LiveServerTestCase is used to run the django application on an alternative thread, while tests are being executed
@@ -15,33 +17,30 @@ from seleniumbase import BaseCase
 DEMO = False
 
 
-
-class TestBasicTemplate(LiveServerTestCase, BaseCase):
+class TestSuiteTemplate(LiveServerTestCase, BaseCase):
     """ Testing Suite Class - Implement any tests inside its own method - (Methods to be tests must start with "test_*") """
-
-    # def setup(self, method=None):
-    #     print(
-    #         "[Test]: Running SeleniumBase Commands Now" \
-    #         f"LiveServer running on: {self.live_server_url}"
-    #         )
-    #     if method:
-    #         method()
-
+    
+    @pytest.fixture
+    def setup(self):
+        self.demo_mode = DEMO
+        self.headless = (DEMO is False)
+        print(f"[Test] Mode = {'DEMO' if DEMO else 'Headless'}")
 
     # Basic test examples
     def test_example_pass(self):
-        # PASS expected
         self.demo_mode = DEMO
         self.headless = (DEMO is False)
 
+        # PASS expected
         self.open(self.live_server_url)
         self.assert_true("Home" in self.get_page_title(), msg="[TESTING CODE ERROR]: Not on Home-Page - Title does not match")
-        
-            
+        self.click("/html/body/nav/div[2]/div[2]/div/div/div/a[1]")
+
+
     def test_example_fail(self):
-        # FAIL expected
         self.demo_mode = DEMO
         self.headless = (DEMO is False)
-
+        
+        # FAIL expected
         self.open(self.live_server_url)
         self.assert_true("This is Not In Title" in self.get_page_title(), msg="[TESTING CODE ERROR]: Not on Home-Page - Title does not match")
