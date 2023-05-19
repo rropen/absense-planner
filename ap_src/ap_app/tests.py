a7b2540587dcaec412bbc9cb6f348dfff52d9378
Jai Rai 829138 <829138@student.derby-college.ac.uk>
2023-05-19 14:25:34 +0100

    updated tests

--- ap_src/ap_app/tests.py
+++ ap_src/ap_app/tests.py
@@ -1,5 +1,6 @@
-import pytest
+import pytest, names
 from django.test import TestCase
+from parameterized import parameterized
 from seleniumbase import BaseCase
 
 # constants for correct credentials
@@ -14,10 +15,8 @@ PASSWORD_ID2 = "#id_password2"
 
 
 
+class rr_test_cases(BaseCase):
 
-@pytest.mark.order(index=1)
-class TestSignup(BaseCase):
-    """Conatins all tests for the signup page"""
     def auto_signup(self, username = CORRECT_USERNAME, password = CORRECT_PASSWORD):
         """Used a template for typing values in the signup page when testing,
             if not entered the username and password will default to successful values"""
@@ -25,90 +24,50 @@ class TestSignup(BaseCase):
         self.type(PASSWORD_ID1, password)
         self.type(PASSWORD_ID2, password)
         self.click('button:contains("Sign Up")')
-
-    def test_nav(self):
+    
+    @parameterized.expand([["#signup","Sign Up - RR Absence"],["#login", "Login - RR Absence"]])
+    def test_nav(self, id, title):
         # opens the website to the home page
         self.open("http://127.0.0.1:8000")
         
         # clicks Signup button
-        self.click("#signup")
+        self.click(id)
         
         # checks if the page is loaded
-        self.assert_title("Sign Up - RR Absence")
+        self.assert_title(title)
 
-    def test_presence(self):
+    @parameterized.expand([["accounts/login"],["signup"]])
+    def test_presence_check(self, page):
         # opens the website to the signup page
-        self.open("http://127.0.0.1:8000/signup/")     
-        
-        # presence check
-        self.auto_signup(username="", password = "")
+        self.open(f"http://127.0.0.1:8000/{page}")     
+        if "login" in page:
+            self.type(USERNAME_ID, "")
+            self.type(PASSWORD_ID, "") 
+        else:
+            self.auto_signup(username="", password = "")
         
         # A django error appears which cannot be checked so
         # checking the credentials have not been allowed with the lack of a redirect
         self.assert_url("http://127.0.0.1:8000/signup/")
 
-    def test_username(self):
-        # opens the website to the signup page
-        self.open("http://127.0.0.1:8000/signup/")  
-
-        # type checks
-
-        self.auto_signup(username="!")
-        # checking the error message
-        self.assert_text("Enter a valid username") 
-
-        self.auto_signup(username=":")
-        # checking for error text
-        self.assert_text("Enter a valid username") 
-
-        self.auto_signup(username="%")
-        # checking for error text
-        self.assert_text("Enter a valid username")
-
-    def test_password(self):
+    @parameterized.expand([["a","password is too short"],["password","password is too common"],["12345678", "password is entirely numeric"], ["testuser"," password is too similar to the username"]])
+    def test_signup_password(self, password, err_msg):
         # opens the website to the signup page
         self.open("http://127.0.0.1:8000/signup/")  
 
         # testing password validation 
-
-        self.auto_signup(password="a")
+        self.auto_signup(password=password)
         # checking the credentials have not been allowed by making sure it has not left the page
-        self.assert_url("http://127.0.0.1:8000/signup/")
+        self.assert_text(err_msg)
 
-    def test_correct(self):
+    def test_signup_correct(self):
         # opens the website to the signup page
         self.open("http://127.0.0.1:8000/signup/")  
 
         # enters correct credentials
         self.auto_signup()
-
-
-class TestLogin(BaseCase):
-    """Contains all tests for login page"""
-    def test_nav(self):
-
-        # opens the website to the home page
-        self.open("http://127.0.0.1:8000")
         
-        # clicks login button
-        self.click("#login")
-        
-        # checks if the page is loaded
-        self.assert_title("Login - RR Absence")
-
-    def test_presence(self):
-        # opens the website to the signup page
-        self.open("http://127.0.0.1:8000/accounts/login/")     
-        
-        # presence check
-        self.type(USERNAME_ID, "")
-        self.type(PASSWORD_ID, "")
-        
-        # A django error appears which cannot be checked so
-        # checking the credentials have not been allowed with the lack of a redirect
-        self.assert_url("http://127.0.0.1:8000/accounts/login/")
-        
-    def test_incorrect(self):
+    def test_login_incorrect(self):
         # opens the website to the login page
         self.open("http://127.0.0.1:8000/accounts/login")
 
@@ -124,7 +83,7 @@ class TestLogin(BaseCase):
         # check for error message 
         self.assert_text("Please enter a correct username and password", ".message")
         
-    def test_correct(self):
+    def test_login_correct(self):
         # opens the website to the login page
         self.open("http://127.0.0.1:8000/accounts/login")
 
@@ -134,9 +93,9 @@ class TestLogin(BaseCase):
         
         # submits form
         self.click('button:contains("Login")')
-        
-class test_add_absence(BaseCase):
-    def test_add_normal(self):
+        self.assert_url("http://127.0.0.1:8000/")
+
+    def test_add_absence(self):
         self.open("http://127.0.0.1:8000/absence/add")
         self.type(USERNAME_ID, CORRECT_USERNAME)
         self.type(PASSWORD_ID, CORRECT_PASSWORD)
@@ -155,11 +114,11 @@ class test_add_absence(BaseCase):
         self.click("#submit")
         self.assert_text("Absence successfully recorded")
 
-
+    @pytest.mark.xfail
     def test_add_recurring(self):
         pass
 
-class rr_test_cases(BaseCase):
+
 
     def test_remove_member(self):
         self.open("http://127.0.0.1:8000/")
--- ap_src/ap_site/context_processors.py
+++ ap_src/ap_site/context_processors.py
@@ -1,4 +1,6 @@
-from django.conf import settings # This will import the settings file that django uses.
+from django.conf import \
+    settings  # This will import the settings file that django uses.
+
 
 def production(request): # Function that will return the PRODUCTION_UI value.
     return {'PRODUCTION_UI': settings.PRODUCTION_UI}
