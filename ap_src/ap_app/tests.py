1e53b85c480bd4f1e98b3ab0f33854644960000a
Patrick-Wigley <patrickwigley2@gmail.com>
2023-05-26 11:53:16 +0100

    Final Changes before merge

--- ap_src/ap_app/tests.py
+++ ap_src/ap_app/tests.py
@@ -24,57 +24,18 @@ DEMO = True
 
 class TestSuiteTemplate(LiveServerTestCase, BaseCase):
     """ Testing Suite Class - Implement any tests inside
-    its own method - (Methods to be tests must start with "test_*") """
+    its own method - (Methods to be tested must start with "test_*") """
     
 
-    # def test_element_presence(self):
-    #     """ This test will determine """
-    #     self.demo_mode = DEMO
-    #     TITLE_XPATH = "/html/body/section/div/p[1]"
 
-    #     self.open(self.live_server_url)
-    #     self.assert_element_present(TITLE_XPATH)
-
-
-    def test_presence_check_home_page(self):
+# Demo test example
+    def test_example(self):
         self.demo_mode = DEMO
+        TITLE_PATH = "/html/body/section/div/p[1]"
+        
         self.open(self.live_server_url)
-        TITLE_XPATH = "/html/body/section/div/p[1]"
-        SIGNUP_BUTTON_XPATH = "/html/body/container/div/container/div/a" 
-
-        self.assert_element_present(TITLE_XPATH)
-        self.click(SIGNUP_BUTTON_XPATH)
-
-
-    def test_click_signup_from_home(self):
-        self.demo_mode = DEMO
-        self.open(self.live_server_url + "/signup")
-
-
-        USERNAME_ENTRY = "/html/body/container/div/div/form/div[1]/div/input"
-        PASSWORD_ENTRY = "/html/body/container/div/div/form/div[2]/div/input"
-        PASSCONFIRM_ENTRY = "/html/body/container/div/div/form/div[3]/div/input"
-        self.send_keys(USERNAME_ENTRY, text="user")
-        self.send_keys(PASSWORD_ENTRY, text="pass")     
+        self.assert_element_present(TITLE_PATH)
         
 
 
-    # def test_screen_shot_signup(self):
-    #     self.demo_mode = DEMO
-    #     self.open(self.live_server_url + "/signup")
-        
-
-    #     self.save_page_source("signup-HTML-source")
-    #     self.save_screenshot("signup-HTML-source-img")
-
-
-# Demo test examples - (These are skipped)
-    # def test_example_pass(self):
-    #     self.demo_mode = DEMO
-
-    #     # PASS expected
-    #     self.open(self.live_server_url)
-    #     self.assert_true("Home" in self.get_page_title(),
-    #         msg="[ERROR]: Not on Home-Page - Title does not match")
-        
-    
+    # ADD YOUR TESTS HERE
