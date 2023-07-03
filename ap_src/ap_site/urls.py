bfb809fb6a9825c7f9cf37f6b3fd6ce1ee09b8cf
Josh Clarke <867589@student.derby-college.ac.uk>
2023-07-03 17:11:23 +0100

    Login fix

--- ap_src/ap_site/urls.py
+++ ap_src/ap_site/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/login", auth_views.LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path("", include("ap_app.urls")),
     
 ]
