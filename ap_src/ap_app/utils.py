9edbab4f1b83d7ab93d3cbbcba3477676fd675fc
tdljayden <110114652+tdljayden@users.noreply.github.com>
2024-02-22 12:09:11 +0000

    Add function to grab a list of users with switch perms

--- ap_src/ap_app/utils.py
+++ ap_src/ap_app/utils.py
@@ -42,3 +42,19 @@ def grab_users_sharing_teams(request):
     users_sharing_teams = set(users_sharing_teams)
 
     return users_sharing_teams
+
+@login_required
+def grab_users_with_perms(request):
+    current_username = request.user.username
+    user_profiles_with_perms = UserProfile.objects.filter(edit_whitelist__username=current_username)
+    user_ids_with_perms = user_profiles_with_perms.values_list("user_id", flat=True)
+    user_ids_with_perms = list(user_ids_with_perms)
+
+    usernames_with_perms = []
+    for user_id in user_ids_with_perms:
+        user_matching_user_id = User.objects.filter(id=user_id)
+        username_matching_user_id = user_matching_user_id.values_list("username", flat=True)
+        username_matching_user_id = str(username_matching_user_id[0])
+        usernames_with_perms.append(username_matching_user_id)
+    
+    return usernames_with_perms
