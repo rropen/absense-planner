882de0e5b502043bdac9a1cba549901d05ec534a
tdljayden <160426159+Jayden876212@users.noreply.github.com>
2024-06-17 15:13:35 +0100

    Amend variable names and use .get instead of .filter

--- ap_src/ap_app/utils/user_profile.py
+++ ap_src/ap_app/utils/user_profile.py
@@ -3,11 +3,10 @@ from ..models import (UserProfile)
 from django.contrib.auth.models import User
 
 def get_user_id_from_username(selected_username):
-    user_matching_user_id = User.objects.filter(username=selected_username)
-    user_id_matching_username = user_matching_user_id.values_list("id", flat=True)
-    user_id_matching_username = int(user_id_matching_username[0])
+    user_matching_username = User.objects.get(username=selected_username)
+    user_id_of_user = int(user_matching_username.id)
 
-    return user_id_matching_username
+    return user_id_of_user
 
 def get_userprofile_id_from_user_id(user_id):
     userprofile_matching_user_id = UserProfile.objects.filter(user_id=user_id)
