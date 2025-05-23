2eca17ba0018ef56e529f4f39547dce58b7a4aa1
Jayden Waghorn <dxmqkgax@duck.com>
2025-05-23 14:52:01 +0100

    Comment context processors with docstrings

--- ap_src/ap_site/context_processors.py
+++ ap_src/ap_site/context_processors.py
@@ -6,15 +6,29 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-def production(request): # Function that will return the PRODUCTION_UI value.
+# Environment Variables
+
+def production(request):
+    """
+    Returns the PRODUCTION_UI environment variable's value.
+    """
     return {'PRODUCTION_UI': settings.PRODUCTION_UI}
 
 def info(request):
+    """
+    Returns the VERSION environment variable's value.
+    """
     return {'VERSION': settings.VERSION}
 
 def team_api_data(request):
+    """
+    Returns the TEAM_DATA_URL environment variable's value.
+    """
     return {'TEAM_DATA_URL': env("TEAM_DATA_URL")}
 
 def url_splitter(request):
+    """
+    Utility that splits URL for use in the header bar.
+    """
     url_parts = request.path.split("/")
     return {"subdirectory_name": url_parts[1]}
