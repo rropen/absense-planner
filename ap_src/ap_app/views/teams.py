89551afa18faf732294512b9445e8b14cd1964e7
Jayden Waghorn <dxmqkgax@duck.com>
2025-05-29 09:03:01 +0100

    Make URL more readable
    Next step is to use a named route from the Team App

--- ap_src/ap_app/views/teams.py
+++ ap_src/ap_app/views/teams.py
@@ -49,7 +49,8 @@ def create_team(request:HttpRequest) -> render:
             # Send a POST request to the API instead of handling the usual model logic,
             # so that the created team is stored on the Team App instead of the Absence Planner.
 
-            response = requests.post(env("TEAM_DATA_URL") + "api/teams/?format=json", data=request.POST)
+            url = env("TEAM_DATA_URL") + "api/teams/"
+            response = requests.post(url, data=request.POST)
             if response.status_code == 200:
                 return redirect("/teams/api-calendar/" + str(response.json()["id"]))
             elif response.status_code == 400:
