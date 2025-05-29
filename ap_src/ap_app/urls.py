01b32b8442c58cd7166b2d03236a02914375b80b
Rebeka Tamberga <rebekatamberga@gmail.com>
2025-05-29 13:50:03 +0100

    403 Permision Denied Error Page
    Added a custom 403 error page. This page will be displayed when a user tries to access a page they do not have permission for.

--- ap_src/ap_app/urls.py
+++ ap_src/ap_app/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     path("remove_lingering_perms", views.remove_lingering_perms, name="remove_lingering_perms"),
     path("jsi18n", JavaScriptCatalog.as_view(packages=['recurrence']), name="javascript-catalog"),
     #Errors
+    path("403", views.Custom403View, name="403"),
     path("404", views.Custom404View, name="404"),
     path("500", views.Custom500View, name="500"),
     path("400", views.Custom400View, name="400"),
