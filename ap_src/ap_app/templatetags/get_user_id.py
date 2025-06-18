1fc3dd363d53dd3ac9e0ee7ec9659883b8bc8bf1
alfie <alfie.annable28@outlook.com>
2025-06-18 09:35:17 +0100

    WIP

--- ap_src/ap_app/templatetags/get_user_id.py
+++ ap_src/ap_app/templatetags/get_user_id.py
@@ -4,28 +4,29 @@ register = template.Library()
 
 @register.simple_tag
 def get_user_id(all_absences, user, current_user, year, month_num, day):
-    # for absence in all_absences:
-    #     print(absence)
-    # return None
-    for absence in all_absences:
-        #print(absence)
-        dates = absence["dates"]
-        print(absence["dates"])
-        for the_date in absence["dates"]:
-            print(the_date)
-            print("")
-            if the_date.year == year and the_date.month == month_num and the_date.day == day:
-                
-                # print(f"absence {date.year}/{date.month}/{date.day} - target: {absence["target_user_id"]} // user: {absence["User_ID"]}")
-                # print(absence)
-                if absence["target_user_id"] != absence["User_ID"]:
-                    return absence["User_ID"]
-                    # print(absence["target_user_id"])
-                    # return absence["User_ID"]
-                else:
-                    return None
 
-            return None
+    for absence in all_absences:
+
+        print("l12", absence["dates"][1])
+        print("l13", type(absence["dates"]))
+        print("l14", len(absence["dates"]))
+
+        for x in range(len(absence["dates"])):
+
+            print("l18", x)
+
+            print("l20", absence["dates"][x])
+
+            if absence["dates"][x].year == year and absence["dates"][x].month == month_num and absence["dates"][x].day == day:
+                
+                # print(f"absence {the_date.year}/{the_date.month}/{the_date.day} - for user: {absence["target_user_id"]} // by user: {absence["User_ID"]}")
+
+                return absence["User_ID"]
+
+            else: 
+                return None
+
+            
         
             
 
