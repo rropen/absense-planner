a97a697e0141326ea303a4f6513145e758e2acb8
birindersaini <birindersaini7@gmail.com>
2025-06-20 15:23:36 +0100

    Use element name instead of id

--- ap_src/ap_app/static/js/login.js
+++ ap_src/ap_app/static/js/login.js
@@ -1,8 +1,6 @@
-function togglePassword() {
-    const passwordInput = document.getElementById("id_password");
-    const icon = document.getElementById("toggleIcon");
-
-    if (!passwordInput) return;
+function togglePassword(element) {
+    const passwordInput = document.getElementsByName("password")[0];
+    const icon = element.querySelector(".toggle-icon")
 
     if (passwordInput.type === "password") {
       passwordInput.type = "text";
