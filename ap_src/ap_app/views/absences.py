483997b14a1bd0871089356a14d321147b822a61
rhys935 <owenrhys299@gmail.com>
2025-07-09 14:22:06 +0100

    Revert "Whiteo v2"

--- ap_src/ap_app/views/absences.py
+++ ap_src/ap_app/views/absences.py
@@ -37,6 +37,7 @@ def profile_page(request):
                 "ap_app/profile.html",
                 {
                     "form": form,
+                    "message": "Successfully switched user",
                     "absences": absences,
                     "users": users,
                     "recurring_absences": rec_absences,
